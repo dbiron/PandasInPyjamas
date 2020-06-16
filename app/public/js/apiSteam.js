a6e7053783efccdcd0b11fc25c6b6e22321b0bdb
Davy Biron <sfu1982@gmail.com>
2020-06-16 17:37:37 +0200

    changement-cle-api

--- app/public/js/apiSteam.js
+++ app/public/js/apiSteam.js
@@ -27,7 +27,7 @@
     
     .forEach(function (player) {
 
-    fetch(`https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=477F61C7E69CBEA6CB680DC7E036CA31&steamid=${player.id}`).then(function(response){
+    fetch(`https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=4D9242E6515C6920479265D9CE8F7828&steamid=${player.id}`).then(function(response){
         return response.json()
     }).then(function (json){
 
