607d464e8ccb25fccff012180285149b9d478bce
Davy Biron <sfu1982@gmail.com>
2020-06-17 14:43:22 +0200

    correction#3

--- app/views/frontOffice/sitemap.php
+++ app/views/frontOffice/sitemap.php
@@ -6,33 +6,26 @@ include 'app/views/layouts/header.php';
 
 <main class="content">
     <!---------- Section Mentions Légales ---------->
-    <section class="legalm">
-        <p>Ce site est édité par la société Pandas In Pyjamas.</p>
-        <p>Siège social : Pandas In Pyjamas, 20 Rue Winston Churchill - 56000 Vannes</p>
-        <p>Capital social : 10000 euros</p>
-        <p>Téléphone : +33 (0)2.97.00.00.00</p>
-        <h2>Responsable de la rédaction</h2>
-        <p>Pandas In Pyjamas</p>
-        <h2>Développement, hébergement</h2>
-        <p>Pandas In Pyjamas – www.pip.com</p>
-        <p>20 Rue Winston Churchill - 56000 Vannes</p>
-        <p>Tél : 08 21 23 03 54</p>
-        <h2>Informatique et liberté</h2>
-        <p>Aucune information personnelle n'est collectée à votre insu. Les informations que vous nous communiquez lors d’une demande de devis, de renseignements ou d’inscription à la newsletter par courrier, par téléphone, par e-mail ou par formulaire sont uniquement destinées au traitement administratif et commercial de votre demande par la société Pandas In Pyjamas.</p>
-        <p>Elles ne font l'objet d'aucune cession à des tiers. Pandas In Pyjamas  traite les informations qui vous concernent avec la plus grande confidentialité.</p>
-        <p>Conformément à la loi « Informatique et Libertés » n°78-17 du 6 janvier 1978, vous disposez d'un droit d'accès, de rectification et d'opposition aux données personnelles vous concernant. Pour l'exercer, il suffit de nous en faire la demande par écrit aux coordonnées suivantes : Pandas In Pyjamas, 20 Rue Winston Churchill - 56000 Vannes, France.</p>
-        <h2>Droit d'auteur / copyright</h2>
-        <p>L'ensemble du contenu du présent site Internet, y compris nom de domaine, marques, logo, texte… est la propriété de la société Pandas In Pyjamas, il est protégé par les lois en vigueur de la législation française sur la propriété intellectuelle.</p>
-        <p>Aucun élément de ce site ne peut être copié, reproduit, détourné ou dénaturé, et ce, sur quelque support que ce soit, sans constituer un acte de contrefaçon au sens des articles L 335-2 et suivants du code de la propriété intellectuelle.</p> 
-        <h2>Accès au site</h2>
-        <p>L'utilisateur de ce site reconnaît disposer de la compétence et des moyens nécessaires pour accéder et utiliser ce site. Pandas In Pyjamas ne saurait être tenu responsable des éléments en dehors de son contrôle et des dommages qui pourraient éventuellement être subis par l'environnement technique de l'utilisateur et notamment, ses ordinateurs, logiciels, équipements réseaux et tout autre matériel utilisé pour accéder ou utiliser le service et/ou les informations.</p>
-        <p>Il est rappelé que le fait d'accéder ou de se maintenir frauduleusement dans un système informatique, d'entraver ou de fausser le fonctionnement d'un tel système, d'introduire ou de modifier frauduleusement des données dans un système informatique constitue des délits passibles de sanctions pénales.</p>
-        <h2>Limitation de responsabilité</h2>
-        <p>Pandas In Pyjamas s'attache à transmettre des informations régulièrement mises à jour, notamment sur les actualités ainsi que sur le contenu des articles. Toutefois des informations erronées ou des omissions peuvent être constatées, suite notamment à des erreurs de saisie ou de mise en page. Si vous en faisiez le constat nous vous invitons à nous le signaler pour que nous puissions procéder à leur rectification.</p>
-        <p>Nous nous réservons par ailleurs le droit de modifier les informations ou les éventuelles offres commerciales fournies par le présent site, dans le cadre de nos opérations d'actualisation et de mise à jour des données, et ce sans préavis.</p>
-        <p>Les liens hypertextes mis en oeuvre au sein du présent site Internet, en direction d'autres sites et/ou de pages personnelles et d'une manière générale vers toutes ressources existantes sur Internet, ne sauraient engager la responsabilité de Pandas In Pyjamas.</p>
-        <p>De même que la société Pandas In Pyjamas  ne pourra en aucune façon être tenue pour responsable des sites ayant un lien hypertexte avec le présent site et décline toute responsabilité quant à leur contenu et à leur utilisation.</p>
-    </section>
+    <section class="sitemap">
+		<h1>Site Map</h1>
+		<h2><a href="https://pandas-in-pyjamas.herokuapp.com">Homepage</a></h2>
+		<ul>            
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/" title="Pandas in Pyjamas - Le site de la Team Esport">Pandas in Pyjamas - Le site de la Team Esport</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/index.php" title="Pandas in Pyjamas - Le site de la Team Esport">Pandas in Pyjamas - Le site de la Team Esport - Index</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/news" title="PIP - Liste des news de la team">PIP - Liste des news de la team</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/team" title="PIP - Les membres de la team">PIP - Les membres de la team</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/about" title="PIP - About-Us">PIP - About-Us</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/contact" title="PIP - Contactez nous">PIP - Contactez nous</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/login" title="PIP - Login">PIP - Login</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/inscription" title="PIP - Inscrivez vous">PIP - Inscrivez vous</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/admin.php?action=admin" title="PIP - Login - Admin">PIP - Login - Admin</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/article&id=3" title="PIP - Article de la Team">PIP - Article de la Team</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/article&id=2" title="PIP - Article de la Team">PIP - Article de la Team</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/article&id=1" title="PIP - Article de la Team">PIP - Article de la Team</a></li>
+			<li class="lpage"><a href="https://pandas-in-pyjamas.herokuapp.com/cgu" title="PIP - CGU">PIP - CGU</a></li>
+			<li class="lpage last-page"><a href="https://pandas-in-pyjamas.herokuapp.com/legalm" title="PIP - Mention Légales">PIP - Mention Légales</a></li>
+		</ul>
+	</section>
 </main>
 
 <!---------- Appel du Footer ---------->
